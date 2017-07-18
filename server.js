b185e88245591f872c82907d781629b4881f5097
Ryan Cleary <rclearyusc@gmail.com>
2017-07-18 20:05:26 +0000

    beginning passport client-side integration

--- server.js
+++ server.js
@@ -2,16 +2,29 @@
 
 var express = require('express'),
 	routes = require('./app/routes/index.js'),
-	mongoose = require('mongoose');
+	mongoose = require('mongoose'),
+	passport = require('passport'),
+	session = require('express-session');
 	
 var app = express();
+require('dotenv').load();
+require('./app/config/passport')(passport);
 
 mongoose.connect('mongodb://localhost:27017/clementinejs');
 
 	app.use('/public', express.static(process.cwd() + '/public'));
 	app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-	routes(app);
+	app.use(session({
+		secret: 'secretClementine',
+		resave: false,
+		saveUninitialized: true
+	}));
+	
+	app.use(passport.initialize());
+	app.use(passport.session());
+
+	routes(app, passport);
 
 	app.listen(process.env.PORT, function () {
 		console.log('Listening on port '+process.env.PORT+'...');
